7106ce79cdcff1e85e122e82d2f242c2be8675a8
Joakim Lahtinen <joakim.lahtinen@sbab.se>
2021-11-06 12:42:59 +0100

    Implement basic events and commands

---
+++ src/main/kotlin/com/lahtinen/domain/Commands.kt
@@ -0,0 +1,9 @@
+package com.lahtinen.domain
+
+import java.time.LocalDateTime
+import java.util.UUID
+
+data class ReportPatientVaccinated(val patientId: UUID, val date: LocalDateTime, val location: String, val doctorName: String)
+data class IssueVaccinationPassport(val patientId: UUID, val issuer: String)
+data class MarkPatientEligibleForVaccinationPassport(val patientId: UUID)
+data class RemovePatientEligibilityForVaccinationPassport(val patientId: UUID)
---
+++ src/main/kotlin/com/lahtinen/domain/Events.kt
@@ -0,0 +1,11 @@
+package com.lahtinen.domain
+
+import java.time.LocalDateTime
+import java.util.UUID
+
+abstract class Event(val aggregateId: UUID, val date: LocalDateTime)
+class PatientVaccinated(patientId: UUID, date: LocalDateTime, val location: String, val doctorName: String) : Event(patientId, date)
+class PatientEligible(patientId: UUID, date: LocalDateTime) : Event(patientId, date)
+class PatientEligibilityExpired(patientId: UUID, date: LocalDateTime) : Event(patientId, date)
+class VaccinationPassportIssued(patientId: UUID, date: LocalDateTime) : Event(patientId, date)
+class VaccinationPassportRevoked(patientId: UUID, date: LocalDateTime) : Event(patientId, date)
